2cba76bd76e5d05f8653ba79a03d545082a9c756
Sam Sverko <sam.sverko@gmail.com>
2020-09-22 11:53:17 -0400

    Initial commit.

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    SQUIDEX_CLIENT_SECRET: process.env.SQUIDEX_CLIENT_SECRET,
+  },
+};
