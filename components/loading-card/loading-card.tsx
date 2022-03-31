6721f06c786ff1a77c60d96767784c0f9d929754
Sam Sverko <sam.sverko@gmail.com>
2022-03-30 21:05:26 -0400

    Style loading card.

--- components/loading-card/loading-card.tsx
+++ components/loading-card/loading-card.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
+import styles from './loading-card.module.scss';
+
 type LoadingCardProps = {
     recipeCount: number
 }
 
 const LoadingCard = ({ recipeCount }: LoadingCardProps) => {
     return (
-        <div>
+        <div className={styles.container}>
             <h2>Fetching all <b>{recipeCount}</b> recipes <em>just for you</em>!</h2>
-
-            <hr />
         </div>
     );
 };
