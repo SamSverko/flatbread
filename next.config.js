3b36318956c1dc83ecf2793404109a9ec9cb0334
Sam Sverko <sam.sverko@gmail.com>
2022-04-10 17:00:40 -0400

    Add basic service-worker. - Add 'next-pwa' package.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
     reactStrictMode: true,
-};
+});
