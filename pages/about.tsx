d86d395e95d4976323afa3d7f4433b01f75594d2
Sam Sverko <sam.sverko@gmail.com>
2022-04-23 11:45:51 -0400

    Build and style NavBar component. - Add icon assets. - Add header to Home page. - Create placeholder About page.

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import type { NextPage } from 'next';
+
+const About: NextPage = () => {
+    return (
+        <>
+            <h1>About</h1>
+        </>
+    );
+};
+
+export default About;
