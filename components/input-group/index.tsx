8cf82435f71f82268a0163fcd87470f041f831c9
Sam Sverko <sam.sverko@gmail.com>
2023-02-21 20:17:26 -0500

    InputGroup: Add button and categories. - Category & SearchResultsCard: Fix word breaks. - SearchCard: Fix input word break.

--- components/input-group/index.tsx
+++ components/input-group/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 
+import Category from '../category';
+
 import styles from './index.module.scss';
 
 type ComponentProps = {
+    button?: string
+    categories?: string[]
     id: string
     label: string
     name: string
@@ -10,11 +14,38 @@ type ComponentProps = {
     type: 'search' | 'submit' | 'text'
 }
 
-const InputGroup = ({ id, label, name, placeholder, type = 'text' }: ComponentProps) => {
+const InputGroup = ({
+    button,
+    categories,
+    id,
+    label,
+    name,
+    placeholder,
+    type = 'text',
+}: ComponentProps) => {
     return (
         <div className={styles.container}>
             <label htmlFor={id}>{label}</label>
-            <input id={id} name={name} placeholder={placeholder} type={type} />
+            {!button && <input id={id} name={name} placeholder={placeholder} type={type} />}
+
+            {button && 
+                <div className={styles['with-button']}>
+                    <input id={id} name={name} placeholder={placeholder} type={type} />
+                    <button type='submit'>{button}</button>
+                </div>
+            }
+
+            {categories &&
+                <ul>
+                    {categories.map((category, index) => {
+                        return (
+                            <li key={`key-category-${category}-${index}`}>
+                                <Category removable text={category} />
+                            </li>
+                        );
+                    })}
+                </ul>
+            }
         </div>
     );
 };
