289d2eaec96d910cde7d0f4719cb28d9e809a025
SamSverko <sam.sverko@gmail.com>
2024-02-21 18:10:02 -0500

    wip: test splash screen image urls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,37 +70,37 @@ export default function RootLayout({
             <head>
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-640x1136.png"
+                    href="/images/splash/launch-640x1136.png"
                     media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
                 />
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-750x1294.png"
+                    href="/images/splash/launch-750x1294.png"
                     media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
                 />
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-1242x2148.png"
+                    href="/images/splash/launch-1242x2148.png"
                     media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
                 />
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-1125x2436.png"
+                    href="/images/splash/launch-1125x2436.png"
                     media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
                 />
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-1536x2048.png"
+                    href="/images/splash/launch-1536x2048.png"
                     media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
                 />
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-1668x2224.png"
+                    href="/images/splash/launch-1668x2224.png"
                     media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
                 />
                 <link
                     rel="apple-touch-startup-image"
-                    href="/apple-touch-startup-image-2048x2732.png"
+                    href="/images/splash/launch-2048x2732.png"
                     media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
                 />
             </head>
