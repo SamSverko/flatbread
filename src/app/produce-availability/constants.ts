043d14cc5c38602bde1c36c6233b5294b603f0d6
SamSverko <sam.sverko@gmail.com>
2024-05-11 13:10:38 -0400

    feat(produce-availability): add more data

--- src/app/produce-availability/constants.ts
+++ src/app/produce-availability/constants.ts
@@ -1,7 +1,7 @@
 import { type ProduceAvailability } from "./types";
 
 export const PRODUCE_AVAILABILITY: ProduceAvailability[] = [
-    // FRUITS
+    // FRUITS #################################################################
     {
         availability: [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12],
         name: "Apples",
@@ -32,7 +32,72 @@ export const PRODUCE_AVAILABILITY: ProduceAvailability[] = [
         name: "Currants",
         type: "fruit",
     },
-    // VEGETABLES
+    {
+        availability: [7, 8],
+        name: "Gooseberries",
+        type: "fruit",
+    },
+    {
+        availability: [8, 9, 10],
+        name: "Grapes",
+        type: "fruit",
+    },
+    {
+        availability: [6, 7],
+        name: "Haskap Berries",
+        type: "fruit",
+    },
+    {
+        availability: [8, 9],
+        name: "Muskmelon",
+        type: "fruit",
+    },
+    {
+        availability: [8, 9],
+        name: "Nectarines",
+        type: "fruit",
+    },
+    {
+        availability: [7, 8, 9],
+        name: "Peaches",
+        type: "fruit",
+    },
+    {
+        availability: [1, 2, 8, 9, 10, 11, 12],
+        name: "Pears",
+        type: "fruit",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Plums",
+        type: "fruit",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Raspberries",
+        type: "fruit",
+    },
+    {
+        availability: [4, 5, 6],
+        name: "Rhubarb",
+        type: "fruit",
+    },
+    {
+        availability: [6, 7, 8, 9, 10],
+        name: "Strawberries (Field)",
+        type: "fruit",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Strawberries (Greenhouse)",
+        type: "fruit",
+    },
+    {
+        availability: [7, 8, 9],
+        name: "Watermelon",
+        type: "fruit",
+    },
+    // VEGETABLES #############################################################
     {
         availability: [8, 9, 10],
         name: "Artichokes",
@@ -66,6 +131,236 @@ export const PRODUCE_AVAILABILITY: ProduceAvailability[] = [
     {
         availability: [6, 7, 8, 9, 10],
         name: "Broccoli",
+        type: "vegetable",
+    },
+    {
+        availability: [9, 10, 11],
+        name: "Brussels Sprouts",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12],
+        name: "Cabbage",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Carrots",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Cauliflower",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Celery",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Chinese Broccoli (White Flower)",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Corn",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10],
+        name: "Cucumber (Field)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Cucumber (Greenhouse)",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Daikon Radish",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10],
+        name: "Edible Amaranth (Hinn Choy)",
+        type: "vegetable",
+    },
+    {
+        availability: [8, 9, 10],
+        name: "Eggplant",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 7, 8, 9, 10, 11, 12],
+        name: "Garlic",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7],
+        name: "Garlic Scapes",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Kale",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 8, 9, 10, 11, 12],
+        name: "Leeks",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10],
+        name: "Lettuce (Assorted)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Lettuce (Greenhouse)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Mushrooms",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10],
+        name: "Mustard Greens (Gai Choy)",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Nappa Cabbage",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Onions (Cooking)",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Onions (Green)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 8, 9, 10, 11, 12],
+        name: "Onions (Red)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 8, 9, 10, 11, 12],
+        name: "Parsnips",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Peas (Green)",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9],
+        name: "Peas (Snow)",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Peppers (Field)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Peppers (Greenhouse)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Potatoes",
+        type: "vegetable",
+    },
+    {
+        availability: [9, 10],
+        name: "Pumpkin",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8],
+        name: "Radicchio",
+        type: "vegetable",
+    },
+    {
+        availability: [5, 6, 7, 8, 9, 10, 11],
+        name: "Radishes",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Rapini",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Rutabaga",
+        type: "vegetable",
+    },
+    {
+        availability: [5, 6, 7, 8, 9, 10],
+        name: "Snow Pea Shoots (Dow Miu)",
+        type: "vegetable",
+    },
+    {
+        availability: [5, 6, 7, 8, 9, 10],
+        name: "Spinach",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Sprouts",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 8, 9, 10, 11, 12],
+        name: "Winter Squash",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10],
+        name: "Summer Squash",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Sweet Potatoes",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Tomatoes (Field)",
+        type: "vegetable",
+    },
+    {
+        availability: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        name: "Tomatoes (Greenhouse)",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Water Spinash (Tung Choy)",
+        type: "vegetable",
+    },
+    {
+        availability: [6, 7, 8, 9, 10, 11],
+        name: "Yow Choy (Yellow Flower)",
+        type: "vegetable",
+    },
+    {
+        availability: [7, 8, 9, 10],
+        name: "Zucchini",
         type: "vegetable",
     },
 ];
