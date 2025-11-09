d1cc06ee53c44b2466bf1c8e5714f216f38d89b0
SamSverko <sam.sverko@gmail.com>
2025-11-09 16:05:56 -0500

    feat: remove Grid usage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,14 @@
-import { Grid, Link as MUILink, Typography } from "@mui/material";
+import { Box, Link as MUILink, Typography } from "@mui/material";
 import Link from "next/link";
 
 import { APP_BAR_HEIGHT, APP_BAR_HEIGHT_LG } from "@/lib/constants";
 
 export default function NotFound() {
     return (
-        <Grid
-            container
+        <Box
             sx={{
                 alignItems: "center",
+                display: "flex",
                 flexDirection: "column",
                 gap: 1,
                 height: `calc(100dvh - ${APP_BAR_HEIGHT}px)`,
@@ -18,16 +18,12 @@ export default function NotFound() {
                 },
             }}
         >
-            <Grid item>
-                <Typography component="h1" variant="h5">
-                    Page Not Found
-                </Typography>
-            </Grid>
-            <Grid item>
-                <Link href="/" legacyBehavior passHref>
-                    <MUILink>Return Home</MUILink>
-                </Link>
-            </Grid>
-        </Grid>
+            <Typography component="h1" variant="h5">
+                Page Not Found
+            </Typography>
+            <Link href="/" legacyBehavior passHref>
+                <MUILink>Return Home</MUILink>
+            </Link>
+        </Box>
     );
 }
