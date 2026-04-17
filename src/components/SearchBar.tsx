8a1f48f89bd81d82c5f63e2065edcfa4bb685033
Sam Sverko <sam.sverko@gmail.com>
2026-04-17 15:15:43 -0400

    feat: add clear search button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 import { STYLE_SPACER } from "../constants";
 
 type SearchBarProps = {
@@ -6,37 +8,65 @@ type SearchBarProps = {
 };
 
 export default function SearchBar({ onChange, value }: SearchBarProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const params = new URLSearchParams(window.location.search);
 
     if (params.get("recipe")) return null;
 
     return (
-        <div
-            style={{
-                bottom: `env(safe-area-inset-bottom, calc(${STYLE_SPACER} * 1))`,
-                left: 0,
-                padding: `0 calc(${STYLE_SPACER} * 2)`,
-                position: "fixed",
-                right: 0,
-            }}
-        >
-            <input
-                enterKeyHint="search"
-                onChange={(event) => onChange(event.target.value)}
-                placeholder="Search..."
+        <div style={{ position: "relative" }}>
+            <div
                 style={{
-                    backdropFilter: "blur(10px)",
-                    background: "rgba(255, 255, 255, 0.8)",
-                    border: "1px solid #ddd",
-                    borderRadius: "100px",
-                    boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.08)",
-                    fontSize: "1rem",
-                    padding: "14px 16px",
-                    width: "100%",
+                    bottom: `env(safe-area-inset-bottom, calc(${STYLE_SPACER} * 1))`,
+                    left: 0,
+                    padding: `0 calc(${STYLE_SPACER} * 2)`,
+                    position: "fixed",
+                    right: 0,
                 }}
-                type="search"
-                value={value}
-            />
+            >
+                <input
+                    enterKeyHint="search"
+                    onChange={(event) => onChange(event.target.value)}
+                    placeholder="Search..."
+                    ref={inputRef}
+                    style={{
+                        backdropFilter: "blur(10px)",
+                        background: "rgba(255, 255, 255, 0.8)",
+                        border: "1px solid #ddd",
+                        borderRadius: "100px",
+                        boxShadow: "0 -2px 10px rgba(0, 0, 0, 0.08)",
+                        fontSize: "1rem",
+                        padding: "14px 16px",
+                        width: "100%",
+                    }}
+                    type="search"
+                    value={value}
+                />
+                {value && (
+                    <button
+                        onClick={() => {
+                            onChange("");
+                            inputRef.current?.focus();
+                        }}
+                        style={{
+                            backdropFilter: "blur(10px)",
+                            border: "none",
+                            borderRadius: "100px",
+                            background: "white",
+                            cursor: "pointer",
+                            fontFamily: "monospace",
+                            fontSize: "1.2rem",
+                            position: "absolute",
+                            right: `calc(${STYLE_SPACER} * 4)`,
+                            top: "50%",
+                            transform: "translateY(-50%)",
+                        }}
+                    >
+                        x
+                    </button>
+                )}
+            </div>
         </div>
     );
 }
